5670a3104628508e655db9fedeef5b7b8e8960f0
Shivam Agrawal <agrawalshivam66@gmail.com>
2018-09-26 14:33:18 +0530

    update

--- other/shivam agrawal 1600338C203.py
+++ other/shivam agrawal 1600338C203.py
@@ -1,15 +1,14 @@
-m=[[1, 1, 1], [1, 1, 0], [1, 0, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1], [0, 0, 0]]
-dis=[[4],[2,4],[2],[0],[6],[4],[4],[2]]
-
 def decision_tree():
     m=[[1, 1, 1], [1, 1, 0], [1, 0, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1], [0, 0, 0]]
     dis=[[4],[2,4],[2],[0],[6],[4],[4],[2]]
-    #sorting
+    
+    #Sorting 
     for i in range (0,len(m)):
-        for j in range(0,i):
-            if dis[i]>dis[j]:
+        for j in range(i+1,len(m)):
+            if dis[i] < dis[j]:
                 dis[i],dis[j]=dis[j],dis[i]
                 m[i],m[j]=m[j],m[i]
+                
     #printing        
     print("Decision tree")
     print("<50\tCOD\tWhole\tDiscount")
@@ -20,6 +19,9 @@ def decision_tree():
 
         
 def decision_table():
+    m=[[1, 1, 1], [1, 1, 0], [1, 0, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 1, 1], [0, 0, 0]]
+    dis=[[4],[2,4],[2],[0],[6],[4],[4],[2]]
+    
     print()
     print("Decision table")
     print("Enter 1 for yes and 0 for No")
@@ -27,6 +29,8 @@ def decision_table():
     c2=int(input("Cash on delivery (1/0) "))
     c3=int(input("Wholesale outlet (1/0) "))
     n=[c1,c2,c3]
+    
+    #comparing
     for i in range(0,len(m)):
         if m[i]==n:
             print("Thee discount rate is ",dis[i])
