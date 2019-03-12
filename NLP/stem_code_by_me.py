bb657e7064da06c3f1cf4ad65770ef09337f46e5
Shivam Agrawal <agrawalshivam66@gmail.com>
2019-03-12 10:41:40 +0530

    stemmer update

--- NLP/stem_code_by_me.py
+++ NLP/stem_code_by_me.py
@@ -5,14 +5,15 @@ Created on Tue Jan 15 10:21:11 2019
 @author: Shivam-PC
 """
 
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet 
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 
 ps = PorterStemmer()
 
 new_text = 'Here is the perfect system for cleaning your room. First, move all of the items that do not have a proper place to the center of the room. Get rid of at least five things that you have not used within the last year. Take out all of the trash, and place all of the dirty dishes in the kitchen sink. Now find a location for each of the items you had placed in the center of the room. For any remaining items, see if you can squeeze them in under your bed or stuff them into the back of your closet. See, that was easy!'
-words = word_tokenize(new_text)
+new_text= new_text.lower()
+words = word_tokenize(new_text.lower())
 
 for w in words:
     print(ps.stem(w), end = ' ')
@@ -20,15 +21,24 @@ for w in words:
 print()
 
 paragraph = new_text.split()
+
 #Dictionary
 for word in paragraph:
     flag = 0
     new_word = word
     for letters in word:
         new_word = new_word[:-1]
-        if len(new_word) < 2:
+        if len(new_word) < 3:
+            print(word, end = ' ')
+            flag=1
             break
-        syn=dictionary.meaning(new_word)
+        
+        if len(new_word) < len(word)-3:
+            print(word, end = ' ')
+            flag=1
+            break
+      
+        syn=wordnet.synsets(new_word)
         if len(syn)!=0:
             flag = 1
             print(new_word, end = ' ')
